baed29664b628b14cd820cd4d10aba219bbb797f
tryal-leaf <katie@parkvue.com>
2021-05-01 08:30:51 -0700

    Add boat restriction to Heavyweight

---
+++ src/main/java/tryalleaf/ravager/mixin/BoatEntityMixin.java
@@ -0,0 +1,26 @@
+package tryalleaf.ravager.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.vehicle.BoatEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import tryalleaf.ravager.power.RavagerPowers;
+
+@Mixin(BoatEntity.class)
+public class BoatEntityMixin {
+
+  @Redirect(
+      method = "interact",
+      at = @At(
+          value = "INVOKE",
+          target = "Lnet/minecraft/entity/player/PlayerEntity;startRiding(Lnet/minecraft/entity/Entity;)Z",
+          ordinal = 0))
+  public boolean preventRiding(PlayerEntity player, Entity entity) {
+    if (RavagerPowers.HEAVYWEIGHT.isActive(player)) {
+      return false;
+    }
+    return player.startRiding(entity);
+  }
+}
